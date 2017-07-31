69f18a1d98705034e4bdb93e54c6a0c5523240ed
jasontianxie <1986tianxie@sina.com>
2017-07-31 11:14:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 const fs = require('fs');
 const join = require('path').join;
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');//使用前先安装mongodb，mongodb的安装可以参考官网，或者是菜鸟教程。mongodb不是用npm安装的。
 const passport = require('passport');
 const config = require('./config');
 
@@ -52,5 +52,5 @@ function listen () {
 
 function connect () {
   var options = { server: { socketOptions: { keepAlive: 1 } } };
-  return mongoose.connect(config.db, options).connection;
+  return mongoose.connect(config.db, options).connection;//用mongoose连接数据库，如果没有要连接的数据库，就新建一个。关于connect的第一个参数，看菜鸟教程上的解释。
 }
