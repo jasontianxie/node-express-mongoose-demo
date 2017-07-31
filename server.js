3958b81ed8f64702c71ee2bf65a4e45e2fe55e24
jasontianxie <1986tianxie@sina.com>
2017-07-31 11:36:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ require('dotenv').config();
 const fs = require('fs');
 const join = require('path').join;
 const express = require('express');
-const mongoose = require('mongoose');//使用前先安装mongodb，mongodb的安装可以参考官网，或者是菜鸟教程。mongodb不是用npm安装的。例子1：http://www.cnblogs.com/jayruan/p/5123754.html
+const mongoose = require('mongoose');//mongoose的使用流程大概是：1、使用schema定义一张表包含哪些字段。2、使用model创建表，3、使用new 方法创建表中的一条数据。
+//使用前先安装mongodb，mongodb的安装可以参考官网，或者是菜鸟教程。mongodb不是用npm安装的。例子1：http://www.cnblogs.com/jayruan/p/5123754.html
 //例子：http://www.cnblogs.com/chuaWeb/p/5174951.html.官网：http://mongoosejs.com/docs/index.html
 const passport = require('passport');
 const config = require('./config');
